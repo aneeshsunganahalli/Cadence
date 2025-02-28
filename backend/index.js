5ccec4b434cdab6d722aa12c2265cb7831567166
Aneesh <aneeshappid@gmail.com>
2025-02-28 22:19:35 +0530

    Added Login function

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,10 +14,16 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 const port = 5000
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/user", userRouter);
+
+app.get('/', (req,res) => {
+  res.send('API WORKING')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
-}
-)
+})
