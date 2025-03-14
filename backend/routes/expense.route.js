3068dbb84e7ccf76ba48e31dcf7e0d049c7e9314
Aneesh <aneeshappid@gmail.com>
2025-03-14 20:45:32 +0530

    Moved /categories above /:id due to interference of the two GET routes

--- backend/routes/expense.route.js
+++ backend/routes/expense.route.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { addExpense, deleteExpense, getExpense, getExpenses, updateExpense } from "../controllers/expense.controller.js";
+import { addExpense, deleteExpense, getCategorySummary, getExpense, getExpenses, updateExpense } from "../controllers/expense.controller.js";
 import authUser from "../middleware/userAuth.js";
 
 const router = express.Router();
 
+router.get("/categories", authUser, getCategorySummary);
 router.post("/", authUser, addExpense);
 router.get("/", authUser, getExpenses);
 router.get("/:id", authUser, getExpense);
 router.delete("/delete/:id", authUser, deleteExpense);
 router.patch("/update/:id", authUser, updateExpense);
+
 
 export default router;
